789a79675428358d4f77f8b0750f725e8c778812
Miguel Aguirre <m.a.a.f1999@hotmail.com>
2024-10-14 18:06:41 -0500

    ultimas actualizaciones

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,22 +1,29 @@
-import { NgModule } from '@angular/core';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import { SharedModule } from '../shared/shared.module';
+
+
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
+import { NgModule } from '@angular/core';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
-import { CardListComponent } from './components/card-list/card-list.component';
+
 
 
 @NgModule({
   declarations: [
+    CardListComponent,
+    GifsCardComponent,
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports:[
-    HomePageComponent,
-    CardListComponent
+    CardListComponent,
+    HomePageComponent
   ]
 })
 export class GifsModule { }
